ba17e1ef53ca536a23de563e688951ad8058e3ec
vinothmanicks <vinoth.manicks@gmail.com>
2016-09-27 22:22:40 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function (req, res) {
+  res.send("Article one would be served on this webpage as soon as it is ready.");
+});
+
+app.get('/article-two', function (req, res) {
+  res.send("Article two would be served on this webpage as soon as it is ready.");
+});
+
+app.get('/article-three', function (req, res) {
+  res.send("Article three would be served on this webpage as soon as it is ready.");
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
