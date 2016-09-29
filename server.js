4362b0bbb63287402358c7336baa7b995fda6718
vinothmanicks <vinoth.manicks@gmail.com>
2016-09-28 22:25:47 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ var articles = {
     }
 };
 
-function createTemplate (data) {
+function createTemplate(data) {
     var title = data.title;
     var heading = data.heading;
     var date = data.date;
